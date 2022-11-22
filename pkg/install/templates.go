de65a74c275a4232b4718775d76a9d34bb082544
Jan Lohage <lohage@23technologies.cloud>
2022-11-22 14:17:26 +0100

    move templates to embedded files
    Signed-off-by: Jan Lohage <lohage@23technologies.cloud>

--- pkg/install/templates.go
+++ pkg/install/templates.go
@@ -1,9 +1,9 @@
 package install
 
 import (
+	"embed"
 	"fmt"
-
-	yaml "gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 )
 
 func newDomainConfigAzure(domain string) domainConfiguration {
@@ -37,134 +37,26 @@ func createDomainConfiguration(domain string, dnsProvider string) (domainConfigu
 }
 
 func keConfigTpl() []string {
-	return []string{`
-apiVersion: v1
-kind: Secret
-metadata:
-  name: 23ke-config
-  namespace: flux-system
-type: Opaque
-stringData:
-  values.yaml: |
-    clusterIdentity: {{ .ClusterIdentity }}
-    dashboard:
-      clientSecret: {{ .Dashboard.ClientSecret }} 
-      sessionSecret: {{ .Dashboard.SessionSecret }}
-    kubeApiServer:
-      basicAuthPassword: {{ .KubeApiServer.BasicAuthPassword }}
-    issuer:
-      acme:
-        email: {{ .EmailAddress }}
-    domains:
-      global: # means used for ingress, gardener defaultDomain and internalDomain
-        {{- nindent 8 (toYaml .DomainConfig) }}
-`,
-		`
-apiVersion: v1
-kind: Secret
-metadata:
- name: gardener-values
- namespace: flux-system
-type: Opaque
-stringData:
- values.yaml: |
-   global:
-     deployment:
-       virtualGarden:
-         clusterIP: {{ .Gardener.ClusterIP }} 
-`,
-		`
-apiVersion: v1
-kind: Secret
-metadata:
- name: gardenlet-values
- namespace: flux-system
-type: Opaque
-stringData:
- values.yaml: |-
-   config:
-     seedConfig:
-       metadata:
-         name: initial-seed 
-       spec:
-         networks:
-           nodes: {{ .Gardenlet.SeedNodeCidr }} 
-           pods: {{ .Gardenlet.SeedPodCidr }} 
-           services: {{ .Gardenlet.SeedServiceCidr }} 
-           shootDefaults:
-             pods: 100.73.0.0/16
-             services: 100.88.0.0/13
-         provider:
-           region: {{ .BaseCluster.Region }} 
-           type: {{ .BaseCluster.Provider }} 
-         settings:
-           verticalPodAutoscaler:
-             enabled: false # enable if your base cluster does not 
-`,
-		`
-apiVersion: v1
-kind: Secret
-metadata:
-  name: extensions-values
-  namespace: flux-system
-type: Opaque
-stringData:
-  values.yaml: |
-    os-ubuntu:
-      enabled: true
-    os-gardenlinux:
-      enabled: true
-    networking-calico:
-      enabled: true
-    {{- nindent 4 (toYaml .ExtensionsConfig) }}
-`,
-		`apiVersion: v1
-kind: Secret
-metadata:
- name: dashboard-values
- namespace: flux-system
-type: Opaque
-stringData:
- values.yaml: |
-   frontendConfig:
-     seedCandidateDeterminationStrategy: MinimalDistance
-`,
-		`apiVersion: v1
-  kind: Secret
-  metadata:
-    name: identity-values
-    namespace: flux-system
-  type: Opaque
-  stringData:
-    values.yaml: |
-      staticPasswords:
-      - email: {{ .emailAddress }} 
-        hash: {{ bcrypt .AdminPassword }}  
-        username: "admin"
-        userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
-`,
-		`apiVersion: v1
-kind: Secret
-metadata:
- name: cloudprofiles-values
- namespace: flux-system
-type: Opaque
-stringData:
- values.yaml: |
-   global:
-     kubernetes:
-       versions:
-         1.22.9:
-           classification: preview
-     seedSelector:
-       enabled: true
-       selector:
-         providerTypes:
-           - hcloud
-   hcloud:
-     enabled: true
-   betacloud:
-     enabled: true
-`,
+	return []string{
+		readTemplate("23ke-config.yaml"),
+		readTemplate("gardener-values.yaml"),
+		readTemplate("gardenlet-values.yaml"),
+		readTemplate("extensions-values.yaml"),
+		readTemplate("dashboard-values.yaml"),
+		readTemplate("identity-values.yaml"),
+		readTemplate("cloudprofiles-values.yaml"),
 	}
+}
+
+//go:embed templates/*.yaml
+var templates embed.FS
+
+func readTemplate(name string) string {
+	bytes, err := templates.ReadFile("templates/" + name)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(bytes)
 }
