47723b87b1dd6668da8a2d174195945d21fbf691
Jens Schneider <jens.schneider.ac@posteo.de>
2023-01-13 17:07:39 +0100

    wip
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- pkg/install/install_test.go
+++ pkg/install/install_test.go
@@ -3,12 +3,16 @@
 package install_test
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
+
 	install "github.com/23technologies/23kectl/pkg/install"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/spf13/viper"
-	"math/rand"
 )
 
 var testConfig = map[string]any{
@@ -20,9 +24,9 @@ var testConfig = map[string]any{
 	"basecluster.nodecidr":                 "10.250.0.0/16",
 	"basecluster.provider":                 "hcloud",
 	"basecluster.region":                   "hel1",
-	"bucket.accesskey":                     "my-accesskey@exmaple.org",
-	"bucket.endpoint":                      "minio.my-endpoint.example.org",
-	"bucket.secretkey":                     "bXktZXhhbXBsZS1zZWNyZXQta2V5",
+	"bucket.accesskey":                     "Q3AM3UQ867SPQQA43P2F",
+	"bucket.endpoint":                      "play.min.io",
+	"bucket.secretkey":                     "zuf+tfteSlswRu7BJ86wekitnifILbZam1KYY3TG",
 	"clusteridentity":                      "garden-cluster-my-identity",
 	"dasboard.clientsecret":                "bXktZGFzaGJvYXJkLWNsaWVudC1zZWNyZXQ=",
 	"dasboard.sessionsecret":               "bXktZGFzaGJvYXJkLXNlc3Npb24tc2VjcmV0",
@@ -36,12 +40,6 @@ var testConfig = map[string]any{
 		"domain":   "my-domain.example.org",
 		"provider": "azure-dns",
 	},
-	// "domainconfig.credentials.clientid":       "my-client-id",
-	// "domainconfig.credentials.clientsecret":   "my-client-secret",
-	// "domainconfig.credentials.subscriptionid": "my-subscription-id",
-	// "domainconfig.credentials.tenantid":       "my-tenantid",
-	// "domainconfig.domain":                     "my-domain.example.org",
-	// "domainconfig.provider":                   "azure-dns",
 
 	"emailaddress":                     "test@example.org",
 	"gardener.clusterip":               "10.0.0.1",
@@ -58,9 +56,29 @@ var _ = It("Should install", func() {
 	fmt.Println("Using " + configFileName)
 	viper.SetConfigFile(configFileName)
 
-	install.TestConfig = testConfig
 
-	err := install.Install(testKubeConfig)
+	endpoint := testConfig["bucket.endpoint"].(string)
+	accessKeyID := testConfig["bucket.accesskey"].(string)
+	secretAccessKey := testConfig["bucket.secretkey"].(string)
+
+	// Initialize minio client object.
+	s3Client, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: true,
+	})
+	Expect(err).NotTo(HaveOccurred())
+	
+	// Make a new bucket called mymusic.
+	bucketName := fmt.Sprint(rand.Uint32())
+
+	err = s3Client.MakeBucket(context.Background(), bucketName, minio.MakeBucketOptions{})
+
+
+	install.TestConfig = testConfig
+	err = install.Install(testKubeConfig)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = s3Client.RemoveBucket(context.Background(), bucketName)
 	Expect(err).NotTo(HaveOccurred())
 
 	//sourceController := client.ObjectKey{
