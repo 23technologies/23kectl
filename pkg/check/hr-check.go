172fe657c44fa0ea1447c14baea273fa56744dda
Jens Schneider <jens.schneider.ac@posteo.de>
2023-04-28 16:44:04 +0200

    wip
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- pkg/check/hr-check.go
+++ pkg/check/hr-check.go
@@ -1,12 +1,16 @@
 package check
 
 import (
+	"bytes"
 	"context"
+	"os/exec"
 	"regexp"
 	"strings"
 
 	helmv2 "github.com/fluxcd/helm-controller/api/v2beta1"
 	sourcev1 "github.com/fluxcd/source-controller/api/v1beta2"
+	"github.com/mitchellh/go-wordwrap"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -37,27 +41,53 @@ func (d *HelmReleaseCheck) Run() *Result {
 	// parse status from current helm releaes
 	// we assume that we can build our logic on
 	// this status message
+	result.Conditions = *hr.GetStatusConditions()
 	for _, condition := range hr.Status.Conditions {
-		if condition.Type == "Ready" {
-			result.Status = condition.Message
-			break
-		}
-	}
 
-	regexMap := map[*regexp.Regexp]func(res *Result, matches []string){}
-	regexMap[regexp.MustCompile("Release reconciliation succeeded")] = func(res *Result, matches []string) { res.IsError = false; res.IsOkay = true }
-	regexMap[regexp.MustCompile("install retries exhausted|Helm install failed")] = func(res *Result, matches []string) { res.IsError = true; res.IsOkay = false }
-	// https://regex101.com/r/1l7ita/1
-	regexMap[regexp.MustCompile("^HelmChart '(?P<namespace>.*)/(?P<name>.*)' is not ready$")] = handleHelmChartError
+		regexMap := map[*regexp.Regexp]func(res *Result, matches []string){}
+		regexMap[regexp.MustCompile("Release reconciliation succeeded")] = func(res *Result, matches []string) { res.IsError = false; res.IsOkay = true }
+		regexMap[regexp.MustCompile("install retries exhausted|upgrade retries exhausted|Helm install failed")] = func(res *Result, matches []string) { res.IsError = true; res.IsOkay = false }
+		regexMap[regexp.MustCompile("^HelmChart '(?P<namespace>.*)/(?P<name>.*)' is not ready$")] = handleHelmChartError // https://regex101.com/r/1l7ita/1
+		regexMap[regexp.MustCompile("Helm test failed: pod (?P<podName>.*) failed")] = handeHelmTestError
 
-	for curRegexp, curFunc := range regexMap {
-		matches := curRegexp.FindStringSubmatch(result.Status)
-		if matches != nil {
-			curFunc(result, matches)
-			break
+		for curRegexp, curFunc := range regexMap {
+			matches := curRegexp.FindStringSubmatch(condition.Message)
+			if matches != nil {
+				curFunc(result, matches)
+			}
 		}
+
 	}
 	return result
+}
+
+
+// handeHelmTestError ...
+func handeHelmTestError(res *Result, matches []string) {
+
+	var log bytes.Buffer
+	cmd := exec.Command("kubectl", "logs", "-n", "garden", matches[1])
+	cmd.Stdout = &log
+	cmd.Run()
+
+	const replacement = "\n    > "
+
+	var replacer = strings.NewReplacer(
+    "\r\n", replacement,
+    "\r", replacement,
+    "\n", replacement,
+    "\v", replacement,
+    "\f", replacement,
+    "\u0085", replacement,
+    "\u2028", replacement,
+    "\u2029", replacement,
+	)
+
+	newline := "\n  > "
+	res.Status = res.Status + newline + matches[0] + newline + replacer.Replace(wordwrap.WrapString(log.String(), 100))
+
+	res.IsError = true
+	res.IsOkay = false
 }
 
 func handleHelmChartError(res *Result, matches []string) {
--- pkg/check/state.go
+++ pkg/check/state.go
@@ -1,8 +1,13 @@
 package check
 
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
 type Result struct {
-	IsError bool
-	IsOkay  bool
-	Status  string
-	Hint    string
+	IsError    bool
+	IsOkay     bool
+	Status     string
+	Hint       string
+	Conditions []metav1.Condition
 }
