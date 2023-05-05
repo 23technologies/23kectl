cbec0100f78d54846d28576b369985cfdfd164ff
Jens Schneider <jens.schneider.ac@posteo.de>
2023-05-05 10:35:02 +0200

    Clean up a little bit
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- pkg/check/state.go
+++ pkg/check/state.go
@@ -1,13 +1,7 @@
 package check
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 type Result struct {
 	IsError    bool
 	IsOkay     bool
 	Status     string
-	Hint       string
-	Conditions []metav1.Condition
 }
