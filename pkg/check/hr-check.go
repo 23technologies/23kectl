7e63d2cc4d761627f23b96862544b4a52fbc20ee
Jens Schneider <jens.schneider.ac@posteo.de>
2023-05-11 12:04:33 +0200

    Introduce gardenlet check
    Signed-off-by: Jens Schneider <jens.schneider.ac@posteo.de>

--- pkg/check/hr-check.go
+++ pkg/check/hr-check.go
@@ -11,6 +11,8 @@ import (
 	"github.com/mitchellh/go-wordwrap"
 	corev1 "k8s.io/api/core/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type HelmReleaseCheck struct {
@@ -43,17 +45,21 @@ func (d *HelmReleaseCheck) Run() *Result {
 	// the order of processing is important as we prioritize the status messages
 	type handler struct {
 		regex *regexp.Regexp
-		fn    func(res *Result, matches []string)
+		fn    func(name string, res *Result, matches []string)
 	}
 
 	handlers := []handler{
+		{
+			regex: regexp.MustCompile("Helm install failed: timed out waiting for the condition"),
+			fn:    handleHelmInstallTimeoutError,
+		},
 		{
 			regex: regexp.MustCompile("Helm test failed: pod (?P<podName>.*) failed"),
 			fn:    handeHelmTestError,
 		},
 		{
 			regex: regexp.MustCompile("(install retries exhausted|upgrade retries exhausted|Helm install failed|Helm upgrade failed).*"),
-			fn: func(res *Result, matches []string) {
+			fn: func(name string, res *Result, matches []string) {
 				res.Status = prettify(matches[0])
 				res.IsError = true
 				res.IsOkay = false
@@ -65,7 +71,7 @@ func (d *HelmReleaseCheck) Run() *Result {
 		},
 		{
 			regex: regexp.MustCompile("Release reconciliation succeeded"),
-			fn: func(res *Result, matches []string) {
+			fn: func(name string, res *Result, matches []string) {
 				res.Status = prettify(matches[0])
 				res.IsError = false
 				res.IsOkay = true
@@ -79,7 +85,7 @@ func (d *HelmReleaseCheck) Run() *Result {
 		for _, condition := range hr.GetConditions() {
 			matches := curHandler.regex.FindStringSubmatch(condition.Message)
 			if matches != nil {
-				curHandler.fn(result, matches)
+				curHandler.fn(hr.Name, result, matches)
 				return result
 			}
 		}
@@ -89,7 +95,7 @@ func (d *HelmReleaseCheck) Run() *Result {
 }
 
 // handeHelmTestError ...
-func handeHelmTestError(res *Result, matches []string) {
+func handeHelmTestError(name string, res *Result, matches []string) {
 
 	// It seems controller-runtime does not allow to access the logs.
 	// Use kubectl directly for the moment.
@@ -100,36 +106,47 @@ func handeHelmTestError(res *Result, matches []string) {
 		log = fmt.Sprintf("couldn't get pod logs: %s", err)
 	}
 
-	// Do some easy formatting for the moment.
-	// We should definitely look for some package doing the job in the end.
-	const replacement = "\n    > "
-	var replacer = strings.NewReplacer(
-		"\r\n", replacement,
-		"\r", replacement,
-		"\n", replacement,
-		"\v", replacement,
-		"\f", replacement,
-		"\u0085", replacement,
-		"\u2028", replacement,
-		"\u2029", replacement,
-	)
-
-	newline := "\n  > "
-	res.Status = matches[0] + newline + replacer.Replace(wordwrap.WrapString(strings.TrimSpace(log), 100))
-
+	res.Status = matches[0] + indent(wordwrap.WrapString(strings.TrimSpace(log), 100), 4)
 	res.IsError = true
 	res.IsOkay = false
 }
 
-func handleHelmChartError(res *Result, matches []string) {
+func handleHelmInstallTimeoutError(name string, res *Result, matches []string) {
+
+	// implement further checks here by adding other cases
+	// todo: define a cleaner interface for this process
+	switch name {
+	case "internal-gardenlet":
+		test, _ := KubeClientGo.CoreV1().Pods("garden").List(context.Background(), metav1.ListOptions{
+			LabelSelector: "role=gardenlet,app=gardener",
+		})
+		var log string
+		for _, pod := range test.Items {
+			logs, _ := KubeClientGo.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{}).Do(context.Background()).Raw()
+			if !strings.Contains(log, string(logs)) {
+				log += "\n" + string(logs)
+			}
+		}
+
+		res.Status = matches[0] + indent(wordwrap.WrapString(strings.TrimSpace(log), 100), 4)
+		res.IsError = true
+		res.IsOkay = false
+	default:
+		res.Status = prettify(matches[0])
+		res.IsError = true
+		res.IsOkay = false
+	}
+}
+
+func handleHelmChartError(name string, res *Result, matches []string) {
 	namespace := matches[1]
-	name := matches[2]
+	podName := matches[2]
 
 	hc := &sourcev1.HelmChart{}
 
 	err := KubeClient.Get(context.Background(), client.ObjectKey{
 		Namespace: namespace,
-		Name:      name,
+		Name:      podName,
 	}, hc)
 
 	status := matches[0]
@@ -149,4 +166,26 @@ func handleHelmChartError(res *Result, matches []string) {
 func prettify(message string) string {
 	newline := "\n  > "
 	return strings.Replace(message, ": ", newline, -1)
+}
+
+// indent ...
+func indent(in string, n int) string {
+
+	// Do some easy formatting for the moment.
+	// We should definitely look for some package doing the job in the end.
+	in = "\n" + in
+	var replacement = "\n" + strings.Repeat(" ", n) + "> "
+	var replacer = strings.NewReplacer(
+		"\r\n", replacement,
+		"\r", replacement,
+		"\n", replacement,
+		"\v", replacement,
+		"\f", replacement,
+		"\u0085", replacement,
+		"\u2028", replacement,
+		"\u2029", replacement,
+	)
+
+	return replacer.Replace(in)
+
 }
